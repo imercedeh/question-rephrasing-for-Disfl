4d8a00b95987697c7adb7b14e4e927223154c0ee
Mercedeh <Imercedeh@gmail.com>
2025-08-01 15:36:09 -0600

    Modify training code and add testing code and some results.

--- question_rephrasing_using_bart_.py
+++ question_rephrasing_using_bart_.py
@@ -13,7 +13,6 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainer,Tr
 from datasets import load_dataset, DatasetDict
 from datasets import Dataset
 from evaluate import load
-from bert_score import score
 import torch.nn.functional as F
 import numpy as np
 import json
@@ -83,6 +82,7 @@ class DisfluencyCorrector:
         decoded_labels = []
 
         # Decode predictions and references
+        #Do batch processing to avoid memory out of range
         for i in range(0, len(preds), 32):
           batch_preds = preds[i:i+32]
           batch_labels = labels[i:i+32]
@@ -99,10 +99,12 @@ class DisfluencyCorrector:
 
         bleu_score = self.bleu.compute(predictions=decoded_preds, references=decoded_labels)
         bertscore_res = self.bertscore.compute(predictions=decoded_preds, references=decoded_labels, lang="en")
+        EM_score = self.exact_match.compute(predictions=decoded_preds, references=decoded_labels)
 
         return {
             "BLEU": bleu_score['bleu'],
-            'Bertscore Recall':np.mean(bertscore_res['recall']),'Bertscore Precision':np.mean(bertscore_res['precision']),'Bertscore F1':np.mean(bertscore_res['f1'])
+            'Bertscore Recall':np.mean(bertscore_res['recall']),'Bertscore Precision':np.mean(bertscore_res['precision']),'Bertscore F1':np.mean(bertscore_res['f1']),
+            'Exact Match':EM_score['exact_match']
         }
 
     def train_model(self,dataset_path,training_arguments):
@@ -125,90 +127,9 @@ class DisfluencyCorrector:
 
       print("Training started..................................................................")
       self.trainer.train()
-      if self.model_saving_path!="Don't Save": self.trainer.save_model(self.model_saving_path)
-
-
-    def make_predictions(self,testset_path,index):
-        print("Loading dataset from the file.....................................................")
-        tokenized_test = self.get_tokenized_dataset(testset_path)
-
-        sources = [item["disfluent"] for item in tokenized_test]
-        references = [item["original"] for item in tokenized_test]
-
-        sources=sources[0:index]
-        references=references[0:index]
-
-        inputs = self.tokenizer(sources, return_tensors="pt", padding='max_length', truncation=True,max_length=self.padding_len)
-
-        self.model.to("cpu")# or "cuda:0"
-        print("Generating predictions............................................................")
-        with torch.no_grad():
-            outputs = self.model.generate(
-                input_ids=inputs["input_ids"],
-                attention_mask=inputs["attention_mask"],
-                max_length=self.padding_len,
-                num_beams=4,
-                early_stopping=True
-            )
-
-        predictions = self.tokenizer.batch_decode(outputs, skip_special_tokens=True,max_length=self.padding_len)
-
-        return predictions, references
-
-    def make_predictions_with_trainer(self,testset_path,index):
-        print("Loading dataset from the file.....................................................")
-        tokenized_test = self.get_tokenized_dataset(testset_path)
-
-        print("Generating predictions............................................................")
-
-        subset = tokenized_test.select(range(index))
-
-        results = self.trainer.predict(subset)
-
-        logits=results.predictions
-        logits=logits[0]
-
-        if isinstance(logits, np.ndarray):
-          logits = torch.tensor(logits)
-
-        probabilities = F.softmax(logits, dim=-1)
-        preds = torch.argmax(probabilities, dim=-1)
-        labels = results.label_ids
-
-
-        decoded_preds = self.tokenizer.batch_decode(preds, skip_special_tokens=True,max_length=self.padding_len)
-        decoded_refs = self.tokenizer.batch_decode(labels, skip_special_tokens=True,max_length=self.padding_len)
-
-        return decoded_preds, decoded_refs
-
-    def evaluate_with_bertscore(self,predictions,references):
-        bertscore_res = self.bertscore.compute(predictions=predictions, references=references, lang="en")
-        R=np.mean(bertscore_res['recall'])
-        P=np.mean(bertscore_res['precision'])
-        F1=np.mean(bertscore_res['f1'])
-
-        print("BERTScore Results:")
-        print(f"Precision: {P:.4f}")
-        print(f"Recall:    {R:.4f}")
-        print(f"F1:        {F1:.4f}")
-
-        return {'AVG Recall':R,'AVG Precision':P,'AVG F1':F1}
-
-    def evaluate_with_exact_match(self,predictions,references):
-        results = self.exact_match.compute(predictions=predictions, references=references)
-
-        print("Exact Match Results:")
-        print(round(results["exact_match"], 4))
-
-        return results["exact_match"]
-
-    def evaluate_model(self,testset_path,index,use_trainer=False):
-      if use_trainer:
-        predictions, targets = self.make_predictions_with_trainer(testset_path,index)
-      else:
-         predictions, targets= self.make_predictions(testset_path,index)
-      self.BERTScores=self.evaluate_with_bertscore(predictions=predictions,references=targets)
-      self.EMScore=self.evaluate_with_exact_match(predictions=predictions,references=targets)
+      if self.model_saving_path!="Don't Save":
+        self.trainer.save_model(self.model_saving_path)
+        self.tokenizer.save_pretrained(self.model_saving_path)
 
 """# Running the code"""
 
@@ -217,17 +138,17 @@ dev_path=root+"Dataset\\dev.json"
 test_path=root+"Dataset\\test.json"
 
 model_checkpoint = "facebook/bart-base"
-model_saving_path=root+"Model\\"
+model_saving_path=root+"Models\\BART"
 
-my_model=DisfluencyCorrector(model_checkpoint,model_saving_path="Don't Save")
+my_model=DisfluencyCorrector(model_checkpoint,model_saving_path=model_saving_path)
 
 training_args = Seq2SeqTrainingArguments(
     output_dir=".\\",
     eval_strategy="epoch",
     save_strategy="epoch",
     learning_rate=5e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
     weight_decay=0.01,
     save_total_limit=1,
     overwrite_output_dir=True,
@@ -246,5 +167,3 @@ training_args = Seq2SeqTrainingArguments(
 )
 
 my_model.train_model(dataset_path={'train':train_path,'dev':dev_path},training_arguments=training_args)
-
-my_model.evaluate_model(test_path,10,True)
